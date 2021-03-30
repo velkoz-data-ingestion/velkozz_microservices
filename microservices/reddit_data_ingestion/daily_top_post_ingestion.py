3df5cbb862303c3840cf3914294709366c180bc1
MatthewTee <teelucksingh.matthew1@gmail.com>
2021-03-30 09:33:54 -0400

    Re-Factored Reddit Pipeline Scheduler to create effective spacing between requests

--- microservices/reddit_data_ingestion/daily_top_post_ingestion.py
+++ microservices/reddit_data_ingestion/daily_top_post_ingestion.py
@@ -23,19 +23,23 @@ config.read("../../service_configs/reddit_services/.reddit.env")
 with open("../../service_configs/reddit_services/reddit_ingestion.txt") as json_file:
     subreddits = json.load(json_file)
 
-# Iterating over the list of subreddits provided and adding jobs to the scheduler:
-for subreddit in subreddits:
-    reddit_scheduler.add_job(
-        RedditContentPipeline,
-        "interval", 
-        minutes=1,
-        args=[subreddit],
-        kwargs={
-            "CLIENT_ID":config["praw_params"]["CLIENT_ID"],
-            "CLIENT_SECRET":config["praw_params"]["CLIENT_SECRET"],
-            "USER_AGENT":config["praw_params"]["USER_AGENT"],
-            "token":config["velkozz_account"]["VELKOZZ_TOKEN"]
-            }
+print("Running Subreddt Data Ingestion for the Following Subreddits:", subreddits)
+
+# Function that writes all data to the database:
+def write_subreddit_data():
+    for subreddit in subreddits:
+        RedditContentPipeline(
+            subreddit,
+            CLIENT_ID = config["praw_params"]["CLIENT_ID"],
+            CLIENT_SECRET = config["praw_params"]["CLIENT_SECRET"],
+            USER_AGENT = config["praw_params"]["USER_AGENT"],
+            token = config["velkozz_account"]["VELKOZZ_TOKEN"]
         )
+        # Sleeping to avoid overloading the REST API:
+        time.sleep(5)
+
+
+# Adding the main function to the Scheduler:
+reddit_scheduler.add_job(write_subreddit_data, "interval", minutes=5)
 
 reddit_scheduler.start()
