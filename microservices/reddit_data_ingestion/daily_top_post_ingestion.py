551a209ba735c88299f782243d4a23f4e9f7593b
MatthewTee <teelucksingh.matthew1@gmail.com>
2021-04-02 00:14:40 -0400

    Functioning reddit microservice

--- microservices/reddit_data_ingestion/daily_top_post_ingestion.py
+++ microservices/reddit_data_ingestion/daily_top_post_ingestion.py
@@ -1,5 +1,5 @@
 # Importing Velkozz APIs:
-from social_media_pipelines.reddit_pipelines import RedditContentPipeline    
+from vdeveloper_api.velkozz_pipelines.social_media_pipelines.reddit_pipelines import RedditContentPipeline
 
 # Importing Scheduling packages:
 import time
@@ -17,10 +17,17 @@ reddit_scheduler = BlockingScheduler()
 
 # Specific Reddit Ingestion dotenv params:
 config = configparser.ConfigParser()
-config.read("../../service_configs/reddit_services/.reddit.env")
+config.read(".reddit.env")
+
+# Extracting config params:
+test_schedule_stat = config["velkozz_account"]["TEST_SCHEDULE"]
+web_api_url = config["velkozz_account"]["VELKOZZ_API_URL"]
+subreddit_lst_file = "reddit_ingestion.txt"
+
+print(f"Reddit Ingestion Script Active: \n -Test Scheduler: {test_schedule_stat} \n -url:{web_api_url} \n -Subreddit List Path: {subreddit_lst_file}")  
 
 # Opening reddit_ingestion.txt file for list of subreddits:
-with open("../../service_configs/reddit_services/reddit_ingestion.txt") as json_file:
+with open(subreddit_lst_file) as json_file:
     subreddits = json.load(json_file)
 
 print("Running Subreddt Data Ingestion for the Following Subreddits:", subreddits)
@@ -33,13 +40,20 @@ def write_subreddit_data():
             CLIENT_ID = config["praw_params"]["CLIENT_ID"],
             CLIENT_SECRET = config["praw_params"]["CLIENT_SECRET"],
             USER_AGENT = config["praw_params"]["USER_AGENT"],
+            VELKOZZ_API_URL = config["velkozz_account"]["VELKOZZ_API_URL"],
             token = config["velkozz_account"]["VELKOZZ_TOKEN"]
         )
         # Sleeping to avoid overloading the REST API:
-        time.sleep(5)
+        time.sleep(10)
 
 
 # Adding the main function to the Scheduler:
-reddit_scheduler.add_job(write_subreddit_data, "interval", minutes=5)
+# Config for rapid development testing:
+if test_schedule_stat == "True": # Boolean read from file is string. Too lazy to convert and deal w/ string2bool.
+    print("Job added with Test Scheduler")
+    reddit_scheduler.add_job(write_subreddit_data, "interval", minutes=2)
+else:
+    print("Job added with Production Scheduler")
+    reddit_scheduler.add_job(write_subreddit_data, "interval", hours=12)
 
 reddit_scheduler.start()
